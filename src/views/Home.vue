5184cc40b5fba83cd570db95bc7f7c7504a95a04
Jackz <jacksonbixby@gmail.com>
2023-02-01 12:11:07 -0600

    Update some texts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,17 +6,17 @@
       <b-button tag="router-link" to="l4d2">View Left 4 Dead 2 Netprops</b-button>
       <b-button tag="router-link" to="csgo">View Counter-Strike: Global Offensive Netprops</b-button>
       <b-button tag="router-link" to="tf2">View Team Fortress 2 Netprops</b-button>
-      <b-button tag="router-link" to="custom">Open Custom URL</b-button>
+      <b-button tag="router-link" to="custom">Upload Custom Netprops</b-button>
     </div>
     <hr />
     <h4 class="title is-4">Outdated?</h4>
     <p>
-      If any netprops are outdated, feel free to email me at <b>me@jackz.me</b>. If you want (it helps), you can update the netprops data yourself, and send that to me, or upload it somewhere to use it in the meantime
+      If any netprops are outdated, feel free to email me at <b>me@jackz.me</b>. If you want (it helps), you can update the netprops data yourself, and send that to me, or upload it to use it in the meantime
     </p>
     <br />
     <h5 class="title is-5">Acquiring Netprops Data</h5>
     <p>This requires to have an up to date sourcemod server</p>
-    <p>Run the command <code>sm_dump_netprops_xml tf2.netprops.xml</code>, this will create the <b>tf2.netprops.xml</b> file in your TF2/tf game folder</p>
+    <p>Run the command <code>sm_dump_netprops_xml netprops.xml</code>, this will create the <b>netprops.xml</b> file in your game's inner folder</p>
   </div>
 </template>
 
